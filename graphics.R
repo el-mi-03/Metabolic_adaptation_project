fbf590ca997afc1223a4bf5e2fcf1ae0c973cb20
el-mi-03 <miticeval96@gmail.com>
2023-01-08 16:12:55 +0200

    Finally able to read excel files

--- graphics.R
+++ graphics.R
@@ -1,4 +1,8 @@
 library("tidyverse")
-library(xlsx)
-install.packages("rJava")
+library(readxl)
+install.packages("readxl")
 install.packages("xlsx")
+install.packages("XLConnect")
+library(XLConnect)
+
+data <- read_excel("22_12_22_merged_table.xlsx")
