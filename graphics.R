7130cf243130114a82c5f0a6c26a69b74f157b63
el-mi-03 <miticeval96@gmail.com>
2023-01-09 17:33:06 +0200

    making a colorful graph with error bars

--- graphics.R
+++ graphics.R
@@ -19,9 +19,13 @@ data <- data %>%
     c("carbon_soucre","concentration,mM",NA,"TM concentration","replica"),
     "_"
   )
+
 data <- data[ ,2:7]
 colnames(data) <- c("Time", "carbon_source", "cs_concentration_mM", "TM_concentration","replica","OD")
-
+data$cs_and_conc <- paste(data$carbon_source, "_", data$cs_concentration_mM)
 p <- ggplot(data = data, aes(x = Time, y = OD, group = interaction(carbon_source, cs_concentration_mM))) +
-  geom_line(aes(linetype = cs_concentration_mM), linewidth = 1) + facet_grid(carbon_source~TM_concentration)
+  geom_line(aes(color = cs_and_conc), linewidth = 1, stat = "summary", fun = mean) +
+  geom_errorbar(stat = "summary", fun.data = function(x) {
+    data.frame(ymin = mean(x) - sd(x), ymax = mean(x) + sd(x))
+  }, width = 0.1) + facet_grid(carbon_source~TM_concentration)
 print(p)
