a159a943b0e8c30de89fa22f2033bccc894fdef8
el-mi-03 <miticeval96@gmail.com>
2023-01-09 16:15:44 +0200

    making tidy table from my data

--- graphics.R
+++ graphics.R
@@ -1,10 +1,21 @@
+setwd("H:/Liza/A.baylyi_other_data")
 library(tidyverse)
 library(readxl)
 library(dplyr)
 library(here)
 data <- as_tibble(read_excel("22_12_22_merged_table.xlsx"))
+data <- na.omit(data)
 c <- colnames(data)
 c <- c[!c == "Cycle Nr."]
 c <- c[!c == "Time [s]"]
-data %>% 
+data <- data %>% 
   pivot_longer(c, names_to = "Carbon_source", values_to = "OD")
+
+#a <- data[1:10, ]
+
+data <- data %>%
+  separate(
+    Carbon_source,
+    c("carbon_soucre","concentration,mM",NA,"TM concentration","replica"),
+    "_"
+  )
