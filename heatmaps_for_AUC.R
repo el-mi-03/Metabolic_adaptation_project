102e13db6b92340ac6f4b4e9e4cc300b9b4c9d2b
el-mi-03 <miticeval96@gmail.com>
2023-01-22 14:01:50 +0200

    to make heatmaps out from AUC's calculated for different conditoins

---
+++ heatmaps_for_AUC.R
@@ -0,0 +1,24 @@
+
+setwd("H:/Liza/A.baylyi_other_data")
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+data_name <- "AUC_22_12_22_merged_table"
+data <- as_tibble(read.csv(data_name))
+
+data <- data %>%
+  separate(conditions, into = c('conditions', 'replica'), sep = -2, convert = TRUE) %>%
+  separate(conditions, into = c('Carbon_source', 'TM_conc'), sep = -5, convert = TRUE)
+
+data$replica<-gsub("_","",as.character(data$replica))
+data$TM_conc<-gsub("_","",as.character(data$TM_conc))
+
+data <- data %>% pivot_wider(names_from = replica, values_from = AUC)
+
+data$mean_AUC <- (data$`1`+data$`2`)/2
+data_for_heatmap <- data[c(1,2,5)]
+data_for_heatmap <- data_for_heatmap %>% pivot_wider(names_from = TM_conc, values_from = mean_AUC)
+data_for_heatmap <- data.matrix(data_for_heatmap, rownames.force = NA)
+heatmap(as.matrix(data_for_heatmap))  
+
+        
