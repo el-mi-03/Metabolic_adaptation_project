a0a7fc1821aa26837178d874fddf93f9a682e2d6
el-mi-03 <miticeval96@gmail.com>
2023-01-23 18:06:12 +0200

    spectial edition for my weird tables generated from small reader

--- heatmaps_for_AUC.R
+++ heatmaps_for_AUC.R
@@ -4,9 +4,13 @@ library(tidyverse)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-data_name <- "AUC_22_12_22_merged_table"
+data_name <- "AUC_25_12_22_merged_table_big_reader"
 data <- as_tibble(read.csv(data_name))
+a <- c('..')
+b <- c('z')
+data$conditions <- sapply(data$conditions,  stringi::stri_replace_all_fixed, a, b, vectorize_all = FALSE, USE.NAMES = FALSE)
 
+data$conditions<-gsub(pattern = "z.*","",as.character(data$conditions))
 data <- data %>%
   separate(conditions, into = c('conditions', 'replica'), sep = -2, convert = TRUE) %>%
   separate(conditions, into = c('Carbon_source', 'TM_conc'), sep = -5, convert = TRUE)
@@ -15,7 +19,7 @@ data$replica<-gsub("_","",as.character(data$replica))
 data$TM_conc<-gsub("_","",as.character(data$TM_conc))
 
 data <- data %>% pivot_wider(names_from = replica, values_from = AUC)
-
+data <-na.omit(data)
 data$mean_AUC <- (data$`1`+data$`2`)/2
 data_for_heatmap <- data[c(1,2,5)]
 #data_for_heatmap <- data_for_heatmap %>% pivot_wider(names_from = TM_conc, values_from = mean_AUC)
