4f453cf23d55ba61c9b59cbb934f7ed360a4947d
el-mi-03 <miticeval96@gmail.com>
2023-01-23 17:03:18 +0200

    Finally, nice heatmap!

--- heatmaps_for_AUC.R
+++ heatmaps_for_AUC.R
@@ -3,6 +3,7 @@ setwd("H:/Liza/A.baylyi_other_data")
 library(tidyverse)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 data_name <- "AUC_22_12_22_merged_table"
 data <- as_tibble(read.csv(data_name))
 
@@ -17,11 +18,12 @@ data <- data %>% pivot_wider(names_from = replica, values_from = AUC)
 
 data$mean_AUC <- (data$`1`+data$`2`)/2
 data_for_heatmap <- data[c(1,2,5)]
-data_for_heatmap <- data_for_heatmap %>% pivot_wider(names_from = TM_conc, values_from = mean_AUC)
+#data_for_heatmap <- data_for_heatmap %>% pivot_wider(names_from = TM_conc, values_from = mean_AUC)
 #data_for_heatmap[,2:3] <- sapply(data_for_heatmap[,2:3],fun = as.numeric(sub(",", "\\.", x)))
 
-data_for_heatmap <- data.frame(data_for_heatmap, row.names = 1)
-data_for_heatmap <- data.matrix(data_for_heatmap)
-heatmap(data_for_heatmap)
-
+#data_for_heatmap <- data.frame(data_for_heatmap, row.names = 1)
+#data_for_heatmap <- data.matrix(data_for_heatmap)
+#heatmap(data_for_heatmap)
+ggplot(data_for_heatmap, mapping = aes(x = TM_conc, y = Carbon_source, fill = mean_AUC))+
+  geom_tile()+scale_fill_gradient(high = "orange", low = "blue")
         
